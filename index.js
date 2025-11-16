7d1b6784f7d64d88c15993430dad32d3f6bd2131
kdrtayan-tech <kdrtayan@gmail.com>
2025-11-16 14:59:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Merhaba, bu proje Slack kripto botu için hazırlanıyor.");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Slack kripto bot çalışıyor.");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server çalışıyor → Port: ${PORT}`);
+});
